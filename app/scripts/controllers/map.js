07a7c72f6e1c2182321eff5a48475ee6a1182c38
Kevin Jett <kevjett@gmail.com>
2013-06-01 16:37:48 -0500

    updates for maps

--- app/scripts/controllers/map.js
+++ app/scripts/controllers/map.js
@@ -1,12 +1,15 @@
 'use strict';
 angular.module('NashCivicHackApp')
-  .controller('MapsCtrl', ['$scope','$timeout','$log','$http', function ($scope, $timeout, $log, $http) {
+  .controller('MapsCtrl', ['$scope','$timeout','$log','$http','$routeParams', function ($scope, $timeout, $log, $http,$routeParams) {
 
-  	$http({method: 'GET', url: '/data/130'}).
+  	$http({method: 'GET', url: '/data/' + $routeParams.id}).
 	  success(function(data, status, headers, config) {
 	    console.log('success');
 	    console.log(data);
 	    $scope.markersProperty.push({
+				latitude: 36.08693,
+				longitude: -86.874055
+			},{
 				latitude: 36.149862,
 				longitude: -86.813458
 			});
@@ -23,8 +26,8 @@ angular.module('NashCivicHackApp')
 	  
 	    position: {
 	      coords: {
-	        latitude: 36.149862,
-	        longitude: -86.813458
+	        latitude: 36.166667,
+	        longitude: -86.783333
 	      }
 	    },
 		
@@ -39,19 +42,15 @@ angular.module('NashCivicHackApp')
 		
 		/** list of markers to put in the map */
 		markersProperty: [],
-		/* {
+		 /*{
 				latitude: 36.149862,
 				longitude: -86.813458
 			},
 			{
 				latitude: 36.136887,
 				longitude: -86.806213
-			},
-			{
-				latitude: 36.08693,
-				longitude: -86.874055
-			}],
-		*/
+			}],*/
+		
 		// These 2 properties will be set when clicking on the map
 		clickedLatitudeProperty: null,	
 		clickedLongitudeProperty: null,
